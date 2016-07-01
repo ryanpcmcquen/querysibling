31ff283fe0e497dd247e16d9a357c1cf485f9898
Ryan P.C. McQuen <ryan.q@linux.com>
2016-07-01 07:13:52 -0700

    Initial commit.
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

---
+++ index.js
@@ -0,0 +1,17 @@
+/*! querySibling 1.0.0 by ryanpcmcquen */
+/*global modules*/
+/*jshint esversion:6*/
+const qu = 'querySelector';
+const querySibling = {
+  _qs: (e, s, t) => {
+    (t === 'p') ? (t = 'previous') : (t = 'next');
+    (typeof e === 'string') && (e = document[qu](e));
+    const sa = (e.parentNode[qu + 'All'](s));
+    const ps = (s) => s[t + 'ElementSibling'];
+    return [...sa].filter((i) => (ps(e) === i) ? i : ps(ps(i)))[0];
+  },
+  previous: (e, s) => querySibling._qs(e, s, 'p'),
+  next: (e, s) => querySibling._qs(e, s, 'n')
+};
+
+modules.export = querySibling;
